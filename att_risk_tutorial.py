1d106dc9124a5263ec5f7081e8a7d5bd1a205985
yanchao0222 <34329049+yanchao0222@users.noreply.github.com>
2022-11-17 14:35:28 -0600

    Add files via upload

---
+++ att_risk_tutorial.py
@@ -0,0 +1,197 @@
+import numpy as np
+import time
+import pandas as pd
+import numpy as np
+from scipy import stats
+import os.path
+import sys
+
+'''
+Usage: synthetic_risk_model_attr.py [model_id] [ckpt_id] [x] [y] [original_filename] [output_directory]
+Example: att_risk_tutorial.py 21 best 0 8 /data/chao/syn_mimic/preprocessing/original_training_data.csv result/
+1. [model_id]: model id. Default: 'real'.
+2. [ckpt_id]: checkpoint id of a generative model. Default: 'best'.
+3. [x]: 10 to x is the number of neighbours. A integer larger than -1. Default: '0'. Try: '1'.
+4. [y]: 2 to y is the number of sensitive attributes A integer larger than -1. Default: '8'. Try: '10'.
+5. [original_filename]: the filename of the original patient file.
+6. [output_directory]: output directory. Default: 'result/'.
+'''
+
+def get_entropy(column):
+    (hist, bin_edges) = np.histogram(column, bins=np.arange(min(column), max(column) + 2))
+    hist1 = hist[hist > 0]
+    pk = hist1 / len(column)
+    entropy = -np.sum(pk * np.log(pk))
+    return entropy
+
+
+def find_neighbour(r, r_, data, data_, k, cont_sense_attr):
+    # k: k nearest neighbours
+
+    diff_array = np.abs(data - r)
+    diff_array_max = np.amax(diff_array, axis=0)
+    diff_array_max2 = np.maximum(diff_array_max, 1)
+    diff_array_rate = diff_array/diff_array_max2
+    diff = np.sum(diff_array_rate, axis=1)
+    thresh = np.sort(diff)[k-1]
+    idxs = np.arange(len(data))[diff <= thresh]  # not exactly k neighbours?
+    predict = stats.mode(data_[idxs])[0][0]
+
+    if N_cont > 0:
+        bin_r_ = r_[np.logical_not(cont_sense_attr)]
+        bin_predict = predict[np.logical_not(cont_sense_attr)]
+        cont_r_ = r_[cont_sense_attr]
+        cont_predict = predict[cont_sense_attr]
+        bin_n = len(bin_r_)  # number of binary attributes
+        true_pos = ((bin_predict + bin_r_) == 2)
+        false_pos = np.array([(bin_r_[i] == 0) and (bin_predict[i] == 1) for i in range(bin_n)])
+        false_neg = np.array([(bin_r_[i] == 1) and (bin_predict[i] == 0) for i in range(bin_n)])
+        correct_cont_predict = np.logical_and(cont_predict <= cont_r_ * 1.1, cont_predict >= cont_r_ * 0.9)
+    else:
+        bin_n = len(r_)  # number of binary attributes
+        true_pos = ((predict + r_) == 2)
+        false_pos = np.array([(r_[i] == 0) and (predict[i] == 1) for i in range(bin_n)])
+        false_neg = np.array([(r_[i] == 1) and (predict[i] == 0) for i in range(bin_n)])
+        correct_cont_predict = 0
+    return true_pos, false_pos, false_neg, correct_cont_predict
+
+class Model(object):
+    def __init__(self, fake, n, k, attr_idx):
+        self.fake = fake
+        self.n = n  # number of attributes used by the attacker
+        self.k = k  # k nearest neighbours
+        self.true_pos = []
+        self.false_pos = []
+        self.false_neg = []
+        self.attr_idx = attr_idx  # selected attributes' indexes
+        self.attr_idx_ = np.array([j for j in range(N_attr) if j not in attr_idx])  # unselected attributes' indexes
+        self.data = self.fake[:, self.attr_idx]
+        self.data_ = self.fake[:, self.attr_idx_]
+        if N_cont > 0:
+            self.correct = []
+            self.cont_sense_attr = cont_sense[self.attr_idx_]
+
+    def single_r(self, R):
+        r = R[self.attr_idx]  # tested record's selected attributes
+        r_ = R[self.attr_idx_]  # tested record's unselected attributes
+        if N_cont > 0:
+            true_pos, false_pos, false_neg, correct = find_neighbour(r, r_, self.data, self.data_, self.k, self.cont_sense_attr)
+            self.correct.append(correct)
+        else:
+            true_pos, false_pos, false_neg, _ = find_neighbour(r, r_, self.data, self.data_, self.k, 0)
+        self.true_pos.append(true_pos)
+        self.false_pos.append(false_pos)
+        self.false_neg.append(false_neg)
+
+def cal_score(n, k):
+    # 2^n: the number of attributes used by the attacker
+    # 10^k: the number of neighbours
+
+    real_disease = real[:, SENSE_BEGIN:SENSE_END]
+    disease_attr_idx = np.flipud(np.argsort(np.mean(real_disease, axis=0)))[:2**n]  # sorted by how common a disease is
+
+    attr_idx = np.concatenate([np.array(range(SENSE_BEGIN)), np.array([N_attr - 4]), disease_attr_idx + SENSE_BEGIN]) # move age to the demo block
+    model = Model(fake, 2 ** n, 10 ** k, attr_idx)
+    n_rows = np.shape(real)[0]
+    for i in range(n_rows):
+        if i % 100 == 0:
+            print("patient#: " + str(i))
+        record = real[i, :]
+        model.single_r(record)
+
+    # binary part
+    tp_array = np.stack(model.true_pos, axis=0)  # array of true positives
+    fp_array = np.stack(model.false_pos, axis=0)  # array of false positives
+    fn_array = np.stack(model.false_neg, axis=0)  # array of false negatives
+    tpc = np.sum(tp_array, axis=0)  # vector of true positive count
+    fpc = np.sum(fp_array, axis=0)  # vector of false positive count
+    fnc = np.sum(fn_array, axis=0)  # vector of false negative count
+    f1 = np.nan_to_num(tpc / (tpc + 0.5 * (fpc + fnc)))
+
+    # continuous part
+    if N_cont > 0:
+        correct_array = np.stack(model.correct, axis=0)  # array of correctness
+        accuracy = np.mean(correct_array, axis=0)
+
+    # compute weights
+    entropy = []
+    real_ = real[:, model.attr_idx_]
+    n_attr_ = np.shape(real_)[1]  # number of predicted attributes
+    for j in range(n_attr_):
+        entropy.append(get_entropy(real_[:, j]))
+    weight = np.asarray(entropy) / sum(entropy)
+    if N_cont > 0:
+        bin_weight = weight[np.logical_not(model.cont_sense_attr)]
+        cont_weight = weight[model.cont_sense_attr]
+        score = np.sum(np.concatenate([f1, accuracy]) * np.concatenate([bin_weight, cont_weight]))
+    else:
+        score = np.sum(f1 * weight)
+    return score
+
+
+if __name__ == '__main__':
+    # Default configuration
+    dataset = "mimic" 
+    model_id = "real"
+    ckpt_id = "best"
+    x = 0  # 10 to x is the number of neighbours [0, 1]
+    y = 8  # 2 to y is the number of sensitive attributes used by the attacker [0, 11]
+    original_patient_filename = '/data/chao/syn_mimic/preprocessing/original_training_data.csv'
+    Result_folder = "./result/"
+
+    start1 = time.time()
+    # Enable the input of parameters
+    if len(sys.argv) >= 2:
+        model_id = sys.argv[1]
+    if len(sys.argv) >= 3:
+        ckpt_id = sys.argv[2]
+    if len(sys.argv) >= 4:
+        x = int(sys.argv[3])
+    if len(sys.argv) >= 5:
+        y = int(sys.argv[4])
+    if len(sys.argv) >= 6:
+        original_patient_filename = sys.argv[5]
+    if len(sys.argv) >= 7:
+        Result_folder = sys.argv[6]
+    print("output_directory: " + Result_folder)
+    print("original_filename: " + original_patient_filename)
+    print("syn model_id: " +  model_id)
+    print("x: " + str(x))
+    print("y: " + str(y))
+    if not os.path.exists(Result_folder):
+        os.mkdir(Result_folder)
+
+    SENSE_BEGIN = 8  # first 8 attributes are not sensitive
+    N_attr = 1460  # number of total attributes
+    N_cont = 4  # number of continuous attributes
+
+    SENSE_END = N_attr - N_cont
+    if N_cont > 0:
+        cont_sense = np.array([False for i in range(SENSE_END)] + [True for i in range(N_cont)])
+    exp_name = "Attr_Risk"
+
+    # load datasets
+    real_df = pd.read_csv(original_patient_filename)
+    col_name_list = list(real_df.columns)
+    real = real_df.values
+
+    if model_id == 'real':
+        fake = real
+    else:
+        syn_data = np.load(f'/data/chao/syn_mimic/GAN_training/syn/emrwgan_model_{model_id}_ckpt_{ckpt_id}.npy',allow_pickle=True)
+        for i in range(len(real[0])-4):
+            syn_data[:,i] = (syn_data[:,i] >= 0.5)*1.0
+        syn_data_df = pd.DataFrame(syn_data, columns = col_name_list)
+        positive_outcome_syn_data = syn_data_df[syn_data_df['DIE_1y'] == 1.0].values
+        negative_outcome_syn_data = syn_data_df[syn_data_df['DIE_1y'] == 0.0].values
+        syn_data_df = np.concatenate((positive_outcome_syn_data[:14243,:], negative_outcome_syn_data[:112279,:]), axis=0)
+        syn_data_df = pd.DataFrame(syn_data_df, columns = col_name_list)
+        fake = pd.DataFrame(syn_data_df, columns = col_name_list).values
+        
+    result = cal_score(y, x)
+    elapsed1 = (time.time() - start1)
+    print("Risk: " + str(result) + ".")
+    print("Time used: " + str(elapsed1) + " seconds.")
+    with open(Result_folder + "_" + str(model_id) + 'ckpt_id' + str(ckpt_id) + "_x" + str(x) + "_y" + str(y) + ".txt", 'w') as f:
+        f.write(str(result) + "\n")
+        f.write("Time used: " + str(elapsed1) + " seconds.\n")
---
+++ mem_risk_tutorial.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import numpy as np
+import os.path
+import time
+import sys
+
+'''
+Usage: synthetic_risk_model_mem.py [model_id] [ckpt_id] [theta] [train_filename] [test_filename] [output_directory]
+Example: mem_risk_tutorial.py 21 best 5 /data/chao/syn_mimic/preprocessing/original_training_data.csv /data/chao/syn_mimic/preprocessing/original_testing_data.csv result/
+1. [model_id]: model id. Default: 'real'.
+2. [ckpt_id]: checkpoint id of a generative model. Default: 'best'.
+3. [theta]: the threshold for the euclidean distance between two records. Default: '5'. Try: '10' and '20'.
+4. [train_filename]: the filename of the training file. 
+5. [test_filename]: the filename of the test file. 
+6. [output_directory]: output directory. Default: 'result/'.
+'''
+
+def find_replicant(real, fake):
+    a = np.sum(fake ** 2, axis=1).reshape(fake.shape[0], 1) + np.sum(real.T ** 2, axis=0)
+    b = np.dot(fake, real.T) * 2
+    distance_matrix = a - b
+    return np.min(distance_matrix, axis=0)
+
+def each_group(model):
+    distance_train = np.zeros(n_train)
+    distance_test = np.zeros(n_test)
+    if model_id != 'real':
+        steps = np.ceil(n_train / batchsize)
+        for i in range(int(steps)):
+            distance_train[i * batchsize:(i + 1) * batchsize] = find_replicant(train[i * batchsize:(i + 1) * batchsize], fake)
+
+    steps = np.ceil(n_test / batchsize)
+    for i in range(int(steps)):
+        distance_test[i * batchsize:(i + 1) * batchsize] = find_replicant(test[i * batchsize:(i + 1) * batchsize], fake)
+
+    n_tp = np.sum(distance_train <= theta)  # true positive counts
+    n_fn = n_train - n_tp
+    n_fp = np.sum(distance_test <= theta)  # false positive counts
+    f1 = n_tp / (n_tp + (n_fp + n_fn) / 2)  # F1 score
+    return f1
+
+
+if __name__ == '__main__':
+    # Default configuration
+    dataset = "mimic"
+    model_id = "real"
+    ckpt_id = "best"
+    theta = 5
+    original_patient_filename_train = '/data/chao/syn_mimic/preprocessing/original_training_data.csv'
+    original_patient_filename_test = '/data/chao/syn_mimic/preprocessing/original_testing_data.csv'
+    n_cont_col = 4  # number of columns for continuous features from the right
+    Result_folder = "./result/"
+    if not os.path.exists(Result_folder):
+        os.mkdir(Result_folder)
+    batchsize = 1000
+    exp_name = "Mem_Risk"
+
+    start1 = time.time()
+    # Enable the input of parameters
+    if len(sys.argv) >= 2:
+        model_id = sys.argv[1]
+    if len(sys.argv) >= 3:
+        ckpt_id = sys.argv[2]
+    if len(sys.argv) >= 4:
+        theta = float(sys.argv[3])
+    if len(sys.argv) >= 5:
+        original_patient_filename_train = sys.argv[4]
+    if len(sys.argv) >= 6:
+        original_patient_filename_test = sys.argv[5]
+    if len(sys.argv) >= 7:
+        Result_folder = sys.argv[6]
+    print("output_directory: " + Result_folder)
+    print("train_filename: " + original_patient_filename_train)
+    print("test_filename: " + original_patient_filename_test)
+    print("theta: " + str(theta))
+
+    # load datasets
+    train_df = pd.read_csv(original_patient_filename_train)
+    col_name_list = list(train_df.columns)
+    train = train_df.values
+    
+    test_df = pd.read_csv(original_patient_filename_test)
+    test = test_df.values
+
+    n_train = np.shape(train)[0]
+    n_test = np.shape(test)[0]
+    if model_id == 'real':
+        fake = train.copy()
+    else:
+        syn_data = np.load(f'/data/chao/syn_mimic/GAN_training/syn/emrwgan_model_{model_id}_ckpt_{ckpt_id}.npy',allow_pickle=True)
+        for i in range(len(train[0])-4):
+            syn_data[:,i] = (syn_data[:,i] >= 0.5)*1.0
+        syn_data_df = pd.DataFrame(syn_data, columns = col_name_list)
+        positive_outcome_syn_data = syn_data_df[syn_data_df['DIE_1y'] == 1.0].values
+        negative_outcome_syn_data = syn_data_df[syn_data_df['DIE_1y'] == 0.0].values
+        syn_data_df = np.concatenate((positive_outcome_syn_data[:14243,:], negative_outcome_syn_data[:112279,:]), axis=0)
+        syn_data_df = pd.DataFrame(syn_data_df, columns = col_name_list)
+        fake = pd.DataFrame(syn_data_df, columns = col_name_list).values
+
+    elapsed1 = (time.time() - start1)
+    start2 = time.time()
+    # normalization
+    [n_row, n_col] = fake.shape
+    for j in range(n_col-n_cont_col, n_col):
+        normal_max = np.amax(fake[:, j])
+        normal_min = np.amin(fake[:, j])
+        normal_range = normal_max - normal_min
+        fake[:, j] = (fake[:, j] - normal_min) / normal_range
+        train[:, j] = (train[:, j] - normal_min) / normal_range
+        test[:, j] = (test[:, j] - normal_min) / normal_range
+    result = each_group(model_id)
+    elapsed2 = (time.time() - start2)
+    print("Risk: " + str(result) + ".")
+    print("Time used: " + str(elapsed1 + elapsed2) + " seconds.")
+    print("Loading time used: " + str(elapsed1) + " seconds.")
+    print("Computing time used: " + str(elapsed2) + " seconds.")
+    with open(Result_folder + "_" + str(model_id) + 'ckpt_id' + str(ckpt_id) + "_theta" + str(theta) + ".txt", 'w') as f:
+        f.write(str(result) + "\n")
+        f.write("Time used: " + str(elapsed1 + elapsed2) + " seconds.\n")
+        f.write("Loading time used: " + str(elapsed1) + " seconds.\n")
+        f.write("Computing time used: " + str(elapsed2) + " seconds.\n")
