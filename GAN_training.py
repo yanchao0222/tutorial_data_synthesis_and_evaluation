93d17bea833292dc8593fa614602b5176b72d740
yanchao0222 <34329049+yanchao0222@users.noreply.github.com>
2024-01-18 14:24:56 -0500

    Update GAN_training.py

--- GAN_training.py
+++ GAN_training.py
@@ -153,8 +153,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     parameter_dict = {}
-    parameter_dict['training_data_path'] = '/data/chao/syn_mimic/preprocessing/normalized_training_data.csv'
-    parameter_dict['feature_range_path'] = '/data/chao/syn_mimic/preprocessing/min_max_log.npy'
+    parameter_dict['training_data_path'] = '/YOUR_LOCAL_PATH/normalized_training_data.csv'
+    parameter_dict['feature_range_path'] = '/YOUR_LOCAL_PATH/min_max_log.npy'
     parameter_dict['continuous_feature_col_ind'] = [1456,1457,1458,1459]
     parameter_dict['batchsize'] = 4096
     parameter_dict['Z_DIM'] = 128
